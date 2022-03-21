de6fbc081c5af2cca0d217ceac2e767e302439a8
ruvsphysics <jihosk1995@live.com>
2022-03-21 15:42:25 -0700

    edited

--- shuffle-an-array.js
+++ shuffle-an-array.js
@@ -32,3 +32,20 @@ let arr = [1, 2, 3]
 
 shuffle(arr)
 console.log(arr)
+
+
+//Fisher-Yates shuffle
+//https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle
+
+function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1)); // random index from 0 to i
+  
+      // swap elements array[i] and array[j]
+      // we use "destructuring assignment" syntax to achieve that
+      // you'll find more details about that syntax in later chapters
+      // same can be written as:
+      // let t = array[i]; array[i] = array[j]; array[j] = t
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+  }
